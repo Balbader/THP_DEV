57e70f5177421bb064b8b1d89f377f586c7d18ff
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-26 14:24:23 +0200

    update

--- Week_1/02-Tuesday/Loto/index.js
+++ Week_1/02-Tuesday/Loto/index.js
@@ -1,3 +1,25 @@
+// Define a random number
+const pullRandomNumberInclusive = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+// Create array to store random number for later comparaison
+const storeRandomNumber = () => {
+    let arr = [];
+    while (arr.length != 6) {
+        let nbr = pullRandomNumberInclusive(1, 45);
+        if (!arr.includes(nbr)) {
+            arr.push(nbr);
+        }
+    }
+    return arr;
+}
+
+
+// Function to check if win or lose
 const checkLoto = (firstName, lastName, email, lotoNumber) => {
     const message = document.getElementById('message');
     const emailRegex = new RegExp('([A-Za-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,3})$');
@@ -18,21 +40,7 @@ const checkLoto = (firstName, lastName, email, lotoNumber) => {
         }
     }
     message.innerHTML = "Félicitation ! Vous avez gagné 1 million !!!!!!";
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 // Function to fetch the input information and check its value
@@ -43,4 +51,7 @@ const checkFormSubmit = () => {
     lotoNumber = document.getElementById("#lotoNumber").value;
 
     checkLoto(firstName, lastName, email, lotoNumber.split(","));
-};
+}
+
+// Fetch the input from html form
+document.getElementById('submit').onclick = checkFormSubmit;
