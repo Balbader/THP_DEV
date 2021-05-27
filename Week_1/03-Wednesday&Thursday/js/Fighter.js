15ccc73d542aec7a531ce6f377f407dddde79fc5
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-27 12:10:51 +0200

    update

--- Week_1/03-Wednesday&Thursday/js/Fighter.js
+++ Week_1/03-Wednesday&Thursday/js/Fighter.js
@@ -1,13 +1,13 @@
 class Fighter extends Character {
-    constructor(hp = 50, dmg = 5, mana = 30, state, name = "Pyro-Seth") {
+    constructor(hp = 50, dmg = 5, mana = 150, state, name = "Pyro-Seth") {
         super(hp, dmg, mana, state, name)
     }
 
     // Create Seth special attack
     burnToHell(victim) {
         console.log(`!!! ${this.name} invoked their special attack !!!`);
-        let damage = 20;
+        let damage = 7;
         victim.takeDamage(damage);
-        this.mana = this.mana - 20;
+        this.mana = this.mana - 50;
     }
 }
