98685e1a5ae45ed768ce01af321a8870627d9fd8
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-27 12:43:57 +0200

    update

--- Week_1/03-Wednesday&Thursday/js/Assassin.js
+++ Week_1/03-Wednesday&Thursday/js/Assassin.js
@@ -1,16 +1,16 @@
-class Berzerker extends Character {
-    constructor(hp = 8, dmg = 4, mana = 0, state, name = "Ivar The Berzerker") {
+class Assassin extends Character {
+    constructor(hp = 6, dmg = 6, mana = 20, state, name = "Carl") {
         super(hp, dmg, mana, state, name)
     }
 
     // Create Ivar special attack
-    rage(victim) {
+    shadowHit(victim) {
         console.log(`!!! ${this.name} invoked their special attack !!!`);
-        let damage = 5;
+        let damage = 7;
         victim.takeDamage(damage);
         this.mana = this.mana;
         this.dmg = this.dmg + 1;
-        this.hp = this.hp - 1;
+        this.hp = this.hp;
         /* add last prop:
          * The attack can be invoked as many times as desired
          * Each time it is invoked, it add +1 to Ivar's dmg point
