c34d37887313385474d957dfb9154abdea3ced54
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-29 19:38:50 +0200

    update

--- Week_1/03-Wednesday_Thursday/Berzerker.js
+++ Week_1/03-Wednesday_Thursday/Berzerker.js
@@ -1,21 +1,16 @@
 import Character from "./Character.js";
 
-class Berzerker extends Character {
-    constructor(hp = 8, dmg = 4, mana = 0, state, name = "Ivar The Berzerker") {
+export default class Berzerker extends Character {
+    constructor(hp = 8, dmg = 4, mana = 0, state, name = "Draven") {
         super(hp, dmg, mana, state, name)
     }
 
-    // Create Ivar special attack
+    // Create Draven special attack
     rage(victim) {
         console.log(`!!! ${this.name} invoked their special attack !!!`);
-        let damage = 5;
-        victim.takeDamage(damage);
-        this.mana = this.mana;
-        this.dmg = this.dmg + 1;
-        this.hp = this.hp - 1;
-        /* add last prop:
-         * The attack can be invoked as many times as desired
-         * Each time it is invoked, it add +1 to Ivar's dmg point
-         */
+        if (this.isAlive()) {
+            this.hp -= 1;
+            this.dmg += 1;
+        }
     }
 }
