b8a62609311e30c5dda6eceed91b99199cae17c2
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-29 09:02:34 +0200

    update

---
+++ Week_1/03-Wednesday_Thursday/index.js
@@ -0,0 +1,11 @@
+import Character from "./Character.js";
+import Fighter from "./Fighter.js";
+// import all my files here 
+//
+//
+let grace = new Fighter;
+console.log(grace.name);
+Character.instances.push(grace);
+console.log(Character.instances);
+
+
