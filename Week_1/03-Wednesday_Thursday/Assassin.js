95bce229a47453d7f2ac4bb8230853e676330a36
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-29 20:01:50 +0200

    update

--- Week_1/03-Wednesday_Thursday/Assassin.js
+++ Week_1/03-Wednesday_Thursday/Assassin.js
@@ -1,17 +1,26 @@
 import Character from "./Character.js";
+//import Turn from "./Turn.js";
 
-class Assassin extends Character {
+export default class Assassin extends Character {
     constructor(hp = 6, dmg = 6, mana = 20, state, name = "Carl") {
         super(hp, dmg, mana, state, name)
     }
 
-    // Create Ivar special attack
+    // Create Carl special attack
     shadowHit(victim) {
         console.log(`!!! ${this.name} invoked their special attack !!!`);
-        let damage = 7;
-        victim.takeDamage(damage);
-        this.mana = this.mana;
-        this.dmg = this.dmg + 1;
-        this.hp = this.hp;
+        if (this.mana >= 20 && victim.isAlive()) {
+            this.mana -= 20;
+            victim.takeDamage(7);
+            this.targetPlayer = victim;
+
+            if (this.targetPlayer.state === PLAYING) {
+                this.takeDamage(7);
+                console.log(`${this.name} looses 7 life points as he did not kill ${victim.name}.}`);
+            }
+            return 7;
+        } else {
+             console.log("You do not have enough mana points to invoke this spell.");
+        }
     }
 }
