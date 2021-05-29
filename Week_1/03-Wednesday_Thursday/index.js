4cbe2afadcb8dc3dd285c4c9fc090bc23b8f1d23
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-29 20:01:58 +0200

    update

--- Week_1/03-Wednesday_Thursday/index.js
+++ Week_1/03-Wednesday_Thursday/index.js
@@ -1,7 +1,7 @@
 import Character from "./Character.js";
 import Fighter from "./Fighter.js";
 import Berzerker from "./Berzerker.js";
-//import Assassin from "./Assassin.js";
+import Assassin from "./Assassin.js";
 import Monk from "./Monk.js";
 //import Paladin from "./Paladin.js";
 //import Game from "./Game.js";
@@ -16,5 +16,9 @@ Character.instances.push(draven);
 
 //Monk
 let moana = new Monk;
-Character.instances.push(draven);
+Character.instances.push(moana);
+
+//Assassin
+let carl = new Assassin;
+Character.instances.push(carl);
 console.table(Character.instances);
