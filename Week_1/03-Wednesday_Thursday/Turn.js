9baf4bcd6acdb0b0cee30ccfb68b373b8235f2f4
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-30 12:07:22 +0200

    update

--- Week_1/03-Wednesday_Thursday/Turn.js
+++ Week_1/03-Wednesday_Thursday/Turn.js
@@ -1,4 +1,5 @@
 import Game from "./Game.js";
+import Character from "./Character.js";
 
 export default class Turn extends Game {
     constructor(turnNbr = 1, turnLeft) {
@@ -28,7 +29,53 @@ export default class Turn extends Game {
         alert(`Round ${this.turnNbr}`);
 
         for (let i = playersBatch.length -1; i >= 0; i--) {
+            let playerAttacked = prompt(`It's ${playersBatch[i].name} turn. Who do you want to attack?`);
 
+            while (!this.targetPlayer(playersBatch, i, playerAttacked)) {
+                playerAttacked = prompt(`${playerAttacked} is not in this game. Who do you want to attack?`);
+            }
+            playerAttacked = playersBatch.find(player => player.name.toLowerCase()
+                === playerAttacked.toLowerCase());
+
+            let attackType = prompt(`Do you want to use your special attack? (y/n)`);
+            let dmgNbr;
+
+            if (attackType === 'y') {
+                dmgNbr = playersBatch[i].specialAttack(playerAttacked);
+            } else {
+                dmgNbr = playersBatch[i].dealDamage(playerAttacked);
+            }
+            console.log(`\n ${playersBatch[i].name} is attacking ${playerAttacked.name}. ${playerAttacked.name} recieved ${dmgNbr} damage points and has ${playerAttacked.hp} health points left.`);
         }
+
+        let stillAlive = playersBatch.filter(player => player.isAlive());
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
