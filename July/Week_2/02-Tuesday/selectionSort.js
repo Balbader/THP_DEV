ce910fb317b89afceadca81d9c3822706f0b3c63
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-31 17:47:21 +0200

    update

---
+++ July/Week_2/02-Tuesday/selectionSort.js
@@ -0,0 +1,34 @@
+const swap = (arr, x, y) => {
+    let temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+}
+
+const selectionSort = (array) => {
+    const arr = Array.from(array); // avoid side effects
+    const len = arr.length;
+    let min;
+
+    // remove boundary of unsorten subarray one by one
+    for (let i = 0; i < len - 1; i++) {
+
+        // find the smallest element in the unsorted array
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+
+        // Swap the smallest element with the first element of the array
+        swap(arr, i, min);
+    }
+    return arr;
+}
+
+/*
+let arr = [12, 11, 13, 5, 6 ];
+
+console.log("arr: ", arr);
+console.log(selectionSort(arr));
+*/
