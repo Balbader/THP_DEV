8ee4c1003836ab350d704907a7c869603180ac63
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-01 08:48:42 +0200

    update

--- July/Week_2/02-Tuesday/bubbleSort.js
+++ July/Week_2/02-Tuesday/bubbleSort.js
@@ -1,19 +1,22 @@
-const swap = (arr, x, y) => {
-    let temp = arr[x];
-    arr[x] = arr[y];
-    arr[y] = temp;
-}
+export default class BubbleSort {
 
-const bubbleSort = ( arr, n) => {
-    var i, j;
-    for (i = 0; i < n-1; i++)
-    {
-        for (j = 0; j < n-i-1; j++)
+    swap(arr, x, y) {
+        let temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+    }
+
+    bubbleSort( arr, n) {
+        var i, j;
+        for (i = 0; i < n-1; i++)
         {
-            if (arr[j] > arr[j+1])
+            for (j = 0; j < n-i-1; j++)
             {
-                swap(arr,j,j+1);
+                if (arr[j] > arr[j+1])
+                {
+                    swap(arr,j,j+1);
 
+                }
             }
         }
     }
