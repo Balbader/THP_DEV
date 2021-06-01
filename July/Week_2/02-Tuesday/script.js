6442d7b6da78e0cda6a9fb30a95dcaac9e1f8a60
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-01 08:48:50 +0200

    update

--- July/Week_2/02-Tuesday/script.js
+++ July/Week_2/02-Tuesday/script.js
@@ -1,9 +1,17 @@
 import * as fs from 'fs';
+import BubbleSort from "./bubbleSort.js";
+/*
+import quickSort from "./quickSort.js";
+import selectionSort from "./selectionSort.js";
+import insertionSort from "./insertionSort.js";
+*/
 
-fs.readFile('bubbleSort.js', 'utf8', (err, data) => {
+fs.readFile('list.txt', 'utf8', (err, data) => {
     if (err) {
         console.error(err.message);
         return ;
     }
+    const bubbleSort = new BubbleSort();
+    bubbleSort(data);
     console.log(data);
 });
