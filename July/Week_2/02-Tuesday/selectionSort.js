56a5e90a3279267c4aae7add57f4611256fcf647
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-01 12:57:34 +0200

    update

--- July/Week_2/02-Tuesday/selectionSort.js
+++ July/Week_2/02-Tuesday/selectionSort.js
@@ -1,30 +1,31 @@
-export default class SelectionSort {
+const swap = (arr, x, y) => {
+    let temp = arr[x];
+    arr[x] = arr[y];
+    arr[y] = temp;
+}
 
-    const swap = (arr, x, y) => {
-        let temp = arr[x];
-        arr[x] = arr[y];
-        arr[y] = temp;
-    }
+const selectionSort = (array) => {
+    const arr = Array.from(array); // avoid side effects
+    const len = arr.length;
+    let min;
 
-    const selectionSort = (array) => {
-        const arr = Array.from(array); // avoid side effects
-        const len = arr.length;
-        let min;
+    // remove boundary of unsorten subarray one by one
+    for (let i = 0; i < len - 1; i++) {
 
-        // remove boundary of unsorten subarray one by one
-        for (let i = 0; i < len - 1; i++) {
-
-            // find the smallest element in the unsorted array
-            let min = i;
-            for (let j = i + 1; j < len; j++) {
-                if (arr[j] < arr[min]) {
-                    min = j;
-                }
+        // find the smallest element in the unsorted array
+        let min = i;
+        for (let j = i + 1; j < len; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
             }
-
-            // Swap the smallest element with the first element of the array
-            swap(arr, i, min);
         }
-        return arr;
+
+        // Swap the smallest element with the first element of the array
+        swap(arr, i, min);
     }
+    return arr;
+}
+
+module.exports = {
+    selectionSort,
 }
