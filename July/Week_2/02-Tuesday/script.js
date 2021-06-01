a12bbc1b3b61a722234c5d6d80f87907bf24d71f
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-01 14:23:33 +0200

    update

--- July/Week_2/02-Tuesday/script.js
+++ July/Week_2/02-Tuesday/script.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
-const bubbleSort = require("./bubbleSort.js");
-const insertionSort = require("./insertionSort.js");
-const selectionSort = require("./selectionSort.js");
-const quickSort = require("./selectionSort.js");
 
+const { bubbleSort } = require("./bubbleSort.js");
+const { insertionSort } = require("./insertionSort.js");
+const { selectionSort } = require("./selectionSort.js");
+const { quicksort } = require("./quickSort.js");
 
 const data = fs.readFileSync('list.txt', 'utf8');
 
-console.log(bubble.bubbleSort(data));
+let arr = [48, -2, 6, 12, 0, -4];
+console.log(bubbleSort(arr));
+console.log(insertionSort(arr, 6));
+console.log(selectionSort(arr, 6));
+console.log(quicksort(arr));
