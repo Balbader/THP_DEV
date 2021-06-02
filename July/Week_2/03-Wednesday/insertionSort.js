16d2d28d62c47aac56be8cc19af3e4bc3b6302d4
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-02 07:56:31 +0200

    initial

---
+++ July/Week_2/03-Wednesday/insertionSort.js
@@ -0,0 +1,20 @@
+const insertionSort = (arr, n) => {
+    let key;
+    let j;
+
+    for (let i = 1; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    return arr;
+}
+
+export default insertionSort;
