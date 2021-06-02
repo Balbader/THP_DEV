5c50d168094e1e5c33f6b5654ab534b76abf872a
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-02 14:56:23 +0200

    inital

---
+++ July/Week_2/03-Wednesday/Algorithmes-Part 2/ex-1.js
@@ -0,0 +1,24 @@
+/* Résous le sujet 1 avec une complexité algorithmique de O(n²), c'est-à-dire
+ * que ton programme devra comparer chaque élément entre eux à l'aide
+ * de 2 boucles imbriquées.*/
+
+const equalRes = (arr, res) => {
+    const resultArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === res) {
+                resultArray.push(i);
+                resultArray.push(j);
+                return (`The sum of ${arr[i]} and ${arr[j]} in [${arr}] is equal to ${res}`);
+            }
+        }
+    }
+    if (resultArray.length > 0) {
+        return resultArray;
+    } else {
+        return (`No combination in [${arr}] added up to ${res}.`);
+    }
+}
+
+console.log(equalRes([43, 55, 8, 4], 9));
