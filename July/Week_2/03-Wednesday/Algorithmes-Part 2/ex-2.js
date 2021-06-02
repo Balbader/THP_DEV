616f1e7db3b0205097fc9c9821a7552761e6ed17
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-02 16:55:04 +0200

    update

--- July/Week_2/03-Wednesday/Algorithmes-Part 2/ex-2.js
+++ July/Week_2/03-Wednesday/Algorithmes-Part 2/ex-2.js
@@ -2,7 +2,23 @@
  * que ton programme devra comparer chaque élément entre eux à l'aide
  * de 2 boucles imbriquées. */
 
-const checkHeight = (arr) => {
+const checkHeight = (numbers) => {
+    let hasView = 0;
+    let currentIsMax;
+
+    for (let i = 0; i < numbers.length; i++) {
+        currentIsMax = true;
+        for (let j = i + 1; j < numbers.length; j++) {
+            if (numbers[j] >= numbers[i]) {
+                currentIsMax = false;
+                break;
+            }
+        }
+        if (currentIsMax) {
+            hasView++;
+        }
+    }
+    return hasView;
 }
 
 const arr = [3, 7, 8, 3, 6, 1];
