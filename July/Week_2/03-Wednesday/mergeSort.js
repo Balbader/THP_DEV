6ff906565e6fbcaade3024850c5365c6d83cff1c
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-02 12:56:33 +0200

    update

--- July/Week_2/03-Wednesday/mergeSort.js
+++ July/Week_2/03-Wednesday/mergeSort.js
@@ -3,64 +3,90 @@
 // r = right of arr
 const merge = (arr, l, m, r) => {
 
-    // divide the original array in 2 parts: left array and right array
-    let len1 = m - 1 + 1;
-    let len2 = r - m;
-    const left = [];
-    const right = [];
-    for (let i = 0; i < len1; i++) {
-        left[i] = arr[1 + i];
-    }
-    for (let j = 0; j < len2; j++) {
-        right[i ] = arr[m + 1 + i];
-    }
+    const n1 = m - l + 1;
+    const n2 = r - m;
 
+    // Create temp arrays
+    const L = new Array(n1);
+    const R = new Array(n2);
+
+    // Copy data to temp arrays L[] and R[]
+    for (let i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (let j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+
+    // Merge the temp arrays back into arr[l..r]
+
+    // Initial index of first subarray
     let i = 0;
-    let j = 0;
-    let k = 1;
 
-    // After comparing, we merge the 2 arrays in one sub array
-    while (i < len1 && j < len2) {
-        if (left[i] <= right[j]) {
-            arr[k] = left[i];
+    // Initial index of second subarray
+    let j = 0;
+
+    // Initial index of merged subarray
+    let k = l;
+
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
             i++;
-        } else {
-            arr[k] = arr[i];
+        }
+        else {
+            arr[k] = R[j];
             j++;
         }
         k++;
     }
 
-    // Copy remainig elements of left array, if any
-    while (i < len1) {
-        arr[k] = left[i];
-        k++;
+    // Copy the remaining elements of
+    // L[], if there are any
+    while (i < n1) {
+        arr[k] = L[i];
         i++;
-    }
-
-    // Copy remainig elements of left array, if any
-    while (j < len2) {
-        arr[k] = right[j];
         k++;
+    }
+
+    // Copy the remaining elements of
+    // R[], if there are any
+    while (j < n2) {
+        arr[k] = R[j];
         j++;
+        k++;
     }
 }
 
-/* left = left index of the sub-arr of the array to be sorted
- * right = right index of the sub-arr of the array to be sorted */
-const mergeSort = (arr, left, right) => {
-    if (left < right) {
-        const middle = left + (right - 1) / 2;
-        mergeSort(arr, left, middle);
-        mergeSort(arr, middle + 1, right);
-        merge(arr, left, middle, right);
+// l is for left index and r is
+// right index of the sub-array
+// of arr to be sorted */
+function mergeSort(arr,l, r){
+    if(l>=r){
+        return;//returns recursively
     }
+    const m = l + parseInt((r-l)/2);
+    mergeSort(arr,l,m);
+    mergeSort(arr,m+1,r);
+    merge(arr,l,m,r);
+    return arr;
 }
 
 
+const arrTest = [-2, 7, 15, -14, 0, 15, 0, 7, -7, -4, -13, 5, 8, -14, 12, 123];
+console.log(mergeSort(arrTest, 0, arrTest.length - 1));
 
-const arr = [-2, 7, 15, -14, 0, 15, 0, 7, -7, -4, -13, 5, 8, -14, 12];
-console.log(mergeSort(arr, 0, arr.length));
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
