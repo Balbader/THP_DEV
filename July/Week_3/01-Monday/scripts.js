a0e6474e1aa3ae9aa229e76f5418f66e4c7d4ade
Balbader <15971200+Balbader@users.noreply.github.com>
2021-06-08 21:18:14 +0200

    update

--- July/Week_3/01-Monday/scripts.js
+++ July/Week_3/01-Monday/scripts.js
@@ -1,11 +1,43 @@
-var main_courses = ["Filet de turbot de la mer Noire", "Tablier de sapeur", "Gigot d'agneau", "Faisan de forêt", "Trio de quinoa, chou kale et pousses d'épinard"];
-var techniques = ["à la cocotte", "minute", "avec sa sauce hollandaise", "façon sud-ouest", "comme chez ma grand-mère", "déglacé au saké", "maturé en fût de chêne"];
-var sides = ["une purée de topinambour", "ses frites truffées", "des châtaignes croustillantes", "une brunoise carotte-cèleri", "un oeuf parfait", "sa crème veloutée de fromages affinés"];
-var seasonings = ["au yuzu rouge", "au poivre vert de Sichuan", "et une pointe de safran", "à l'ail noir", "et un peu de sucre en poudre"];
+const main_courses = ["Filet de turbot de la mer Noire", "Tablier de sapeur", "Gigot d'agneau", "Faisan de forêt", "Trio de quinoa, chou kale et pousses d'épinard"];
+const techniques = ["à la cocotte", "minute", "avec sa sauce hollandaise", "façon sud-ouest", "comme chez ma grand-mère", "déglacé au saké", "maturé en fût de chêne"];
+const sides = ["une purée de topinambour", "ses frites truffées", "des châtaignes croustillantes", "une brunoise carotte-cèleri", "un oeuf parfait", "sa crème veloutée de fromages affinés"];
+const seasonings = ["au yuzu rouge", "au poivre vert de Sichuan", "et une pointe de safran", "à l'ail noir", "et un peu de sucre en poudre"];
 
-var random_main_course = main_courses[Math.floor(Math.random()*main_courses.length)];
-var random_technique = techniques[Math.floor(Math.random()*techniques.length)];
-var random_side = sides[Math.floor(Math.random()*sides.length)];
-var random_seasoning = seasonings[Math.floor(Math.random()*seasonings.length)];
+const random_main_course = main_courses[Math.floor(Math.random()*main_courses.length)];
+const random_technique = techniques[Math.floor(Math.random()*techniques.length)];
+const random_side = sides[Math.floor(Math.random()*sides.length)];
+const random_seasoning = seasonings[Math.floor(Math.random()*seasonings.length)];
 
-var menu = `${random_main_course} ${random_technique}, avec ${random_side} ${random_seasoning}`;
+let menu = `${random_main_course} ${random_technique}, avec ${random_side} ${random_seasoning}`;
+
+// Create 'ul' element to display Main Courses on the DOM
+const displayMainCourses = document.querySelector('#tabs');
+
+// Split main_courses arr to append arr element to ul in dom
+const newMainCourses = [];
+
+main_courses.map((element) => {
+    newMainCourses.push(element);
+});
+
+const item = document.createElement('li');
+item.textContent = newMainCourses[0];
+console.log(item);
+
+
+console.log(displayMainCourses);
+
+/*
+for (let i = 0; i < newMainCourses.length; i++) {
+    const item = document.createElement('li');
+    item.textContent = newMainCourses[i];
+    displayMainCourses[0].appendChild(item);
+
+}
+console.log(displayMainCourses);
+*/
+
+
+
+
+
